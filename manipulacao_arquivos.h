63a78751711d180f9dab1a0e0373b5f8daa047be
SamiraFreitas <73719899+SamiraFreitas@users.noreply.github.com>
2021-08-25 20:30:30 -0300

    Delete manipulacao_arquivos.h

--- manipulacao_arquivos.h
+++
@@ -1,9 +0,0 @@
-#ifndef MANIPULACAO_ARQUIVOS_H
-#define MANIPULACAO_ARQUIVOS_H
-
-#include <stdio.h>
-
-/*descobrindo o número de registros em um arquivo*/
-int tamanho_arquivo(FILE *arq);
-
-#endif
